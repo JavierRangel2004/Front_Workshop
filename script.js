46be09154823e94926a1d007022d88d653600ce8
JavierRangel2004 <0256158@up.edu.mx>
2024-03-09 22:35:30 -0600

    css for masonry layout in portraits, experimenting js code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", init);
+
 const allImages = {
   portraits: [],
   landscapes: [],
@@ -5,21 +7,27 @@ const allImages = {
   creativity: [],
 };
 
-for (let i = 1; i <= 70; i++) {
-  allImages.portraits.push(`/media/port/Portrait (${i}).webp`);
-}
-
-for (let i = 1; i <= 50; i++) {
-  allImages.landscapes.push(`/media/land/Landscape (${i}).webp`);
-}
-
-for (let i = 1; i <= 22; i++) {
-  allImages.creativity.push(`/media/creat/Object (${i}).webp`);
-}
-
-for (let i = 1; i <= 30; i++) {
-  allImages.city.push(`/media/city/City (${i}).webp`);
-}
+// Populate image arrays
+["portraits", "landscapes", "city", "creativity"].forEach((category) => {
+  let limit;
+  switch (category) {
+    case "portraits":
+      limit = 70;
+      break;
+    case "landscapes":
+      limit = 50;
+      break;
+    case "city":
+      limit = 30;
+      break;
+    case "creativity":
+      limit = 22;
+      break;
+  }
+  for (let i = 1; i <= limit; i++) {
+    allImages[category].push(`/media/${category}/${category} (${i}).webp`);
+  }
+});
 
 const loadedImages = {
   portraits: 0,
@@ -29,21 +37,26 @@ const loadedImages = {
 };
 
 let galleryLoading = false;
+let msnry; // Masonry instance
 
 function init() {
   if (document.getElementById("featured-photos")) {
     initFeaturedImages();
-    document
-      .getElementById("featured-photos")
-      .addEventListener("wheel", function (event) {
-        if (event.deltaY != 0) {
-          event.preventDefault();
-          this.scrollLeft += event.deltaY;
-        }
-      });
+    handleHorizontalScroll("featured-photos");
   } else {
     initGalleryPage();
   }
+}
+
+function handleHorizontalScroll(elementId) {
+  document
+    .getElementById(elementId)
+    .addEventListener("wheel", function (event) {
+      if (event.deltaY != 0) {
+        event.preventDefault();
+        this.scrollLeft += event.deltaY;
+      }
+    });
 }
 
 function initFeaturedImages() {
@@ -66,63 +79,22 @@ function loadFeaturedImages(sectionId, images, numPhotosPerCategory) {
   });
 }
 
-function initGalleryPage() {
-  const currentCategory = determineCurrentCategory();
-  if (currentCategory) {
-    loadImagesWithAnimation(currentCategory, 15);
-    window.addEventListener("load", function () {
-      function initializeMasonry() {
-        const grid = document.querySelector('.gallery-container');
-        const msnry = new Masonry(grid, {
-          itemSelector: '.img-container',
-          percentPosition: true,
-          columnWidth: '.img-container',
-          gutter: 10
-        });
-      }
-    
-      // Check if Masonry is defined before trying to use it
-      if (typeof Masonry !== 'undefined') {
-        initializeMasonry();
-      } else {
-        console.error('Masonry not loaded');
-      }
-      
-    });
-
-    loadMoreImages(currentCategory); // Pass the currentCategory as argument
-    initInfiniteScroll();
-  }
-}
-
-
-
-function loadMoreImages(category) {
-  const start = loadedImages[category];
-  const end = start + 4;
-  const imagesToLoad = allImages[category].slice(start, end);
-  imagesToLoad.forEach((imageSrc) => {
-    appendImageToSection(category, imageSrc);
-  });
-  loadedImages[category] += imagesToLoad.length;
+function determineCurrentCategory() {
+  const categories = ["portraits", "landscapes", "city", "creativity"];
+  return (
+    categories.find((category) => document.getElementById(category)) || null
+  );
 }
 
 function appendImageToSection(sectionId, imageSrc) {
   const section = document.getElementById(sectionId);
   const container = document.createElement("div");
-  container.classList.add("img-container", "fade-in");
+  container.className = "img-container fade-in";
   const img = document.createElement("img");
   img.src = imageSrc;
-  img.style.opacity = 0;
-  img.classList.add(
-    "img-fluid",
-    "rounded",
-    "shadow-sm",
-    "m-2",
-    "gallery-image"
-  );
+  img.className =
+    "rounded shadow-sm m-2 gallery-image img-fluid img-fluid.loaded";
   img.style.objectFit = "contain";
-
   // Attach event listener for modal
   img.addEventListener("click", () => {
     const modalImage = document.getElementById("modal-image");
@@ -132,17 +104,31 @@ function appendImageToSection(sectionId, imageSrc) {
     );
     modalElement.show();
   });
-
   container.appendChild(img);
   section.appendChild(container);
   fadeInImage(img);
 }
 
-function loadImagesWithAnimation(category, count) {
-  if (loadedImages[category] >= allImages[category].length) {
-    galleryLoading = false;
-    return;
+function fadeInImage(imgElement) {
+  let opacity = 0;
+  const interval = setInterval(() => {
+    opacity += 0.05;
+    imgElement.style.opacity = opacity.toString();
+    if (opacity >= 1) clearInterval(interval);
+  }, 35);
+}
+
+function initGalleryPage() {
+  const currentCategory = determineCurrentCategory();
+  if (currentCategory) {
+    loadImagesWithAnimation(currentCategory, 5, true); // Initial load with Masonry initialization
+    initInfiniteScroll(currentCategory);
   }
+}
+
+function loadImagesWithAnimation(category, count, initializeMasonry = false) {
+  if (galleryLoading || loadedImages[category] >= allImages[category].length)
+    return;
 
   galleryLoading = true;
   const end = Math.min(
@@ -153,11 +139,12 @@ function loadImagesWithAnimation(category, count) {
   for (let i = loadedImages[category]; i < end; i++) {
     const imageSrc = allImages[category][i];
     const img = new Image();
-    img.onload = function () {
-      appendImageToSection(category, this.src);
-      fadeInImage(this);
+    img.onload = () => {
+      appendImageToSection(category, img.src);
       if (i === end - 1) {
         galleryLoading = false;
+        if (initializeMasonry) initializeMasonryLayout(category);
+        else if (msnry) msnry.layout();
       }
     };
     img.src = imageSrc;
@@ -165,42 +152,32 @@ function loadImagesWithAnimation(category, count) {
   loadedImages[category] += count;
 }
 
-function fadeInImage(imgElement) {
-  imgElement.style.opacity = 0;
-  let opacity = 0;
-  const interval = setInterval(() => {
-    if (opacity < 1) {
-      opacity += 0.05;
-      imgElement.style.opacity = opacity;
-    } else {
-      clearInterval(interval);
-    }
-  }, 35);
-}
-
-function initInfiniteScroll() {
-  window.addEventListener("scroll", () => {
-    if (
-      window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 &&
-      !galleryLoading
-    ) {
-      const currentCategory = determineCurrentCategory();
-      if (currentCategory) {
-        galleryLoading = true;
-        loadImagesWithAnimation(currentCategory, 3);
-      }
-    }
+function initializeMasonryLayout(category) {
+  const grid = document.querySelector(`#${category}`);
+  msnry = new Masonry(grid, {
+    itemSelector: ".img-container",
+    percentPosition: true,
   });
 }
 
-function determineCurrentCategory() {
-  const categories = ["portraits", "landscapes", "city", "creativity"];
-  for (const category of categories) {
-    if (document.getElementById(category)) {
-      return category;
-    }
-  }
-  return null;
+function initInfiniteScroll(currentCategory) {
+  let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+  window.addEventListener(
+    "scroll",
+    () => {
+      let st = window.scrollY || document.documentElement.scrollTop;
+      if (st > lastScrollTop) {
+        // downscroll
+        if (
+          window.innerHeight + window.scrollY >=
+            document.body.offsetHeight - 500 &&
+          !galleryLoading
+        ) {
+          loadImagesWithAnimation(currentCategory, 5); // Load more images on scroll
+        }
+      }
+      lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    },
+    false
+  );
 }
-
-document.addEventListener("DOMContentLoaded", init);
